f67c2c8e1263beed35bd4198a867da6a01c86c8d
alexkaglew-ux <alex.kaglew@gmail.com>
2026-09-16 11:40:13 -0700

    Simplify homepage into continuous world

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
 import SiteHeader from "./components/SiteHeader.jsx";
 import Hero from "./components/Hero.jsx";
-import WorldNav from "./components/WorldNav.jsx";
 import PracticeWorlds from "./components/PracticeWorlds.jsx";
 import WatchingRail from "./components/WatchingRail.jsx";
-import SportsNilSection from "./components/SportsNilSection.jsx";
 import IssueExplorer from "./components/IssueExplorer.jsx";
 import InsightsFeed from "./components/InsightsFeed.jsx";
-import LegalAuditModule from "./components/LegalAuditModule.jsx";
 import AboutPreview from "./components/AboutPreview.jsx";
 import BooksEducation from "./components/BooksEducation.jsx";
 import ContactSection from "./components/ContactSection.jsx";
@@ -17,15 +14,12 @@ export default function App() {
     <main>
       <SiteHeader />
       <Hero />
-      <WorldNav />
       <PracticeWorlds />
-      <SportsNilSection />
       <WatchingRail />
       <IssueExplorer />
       <InsightsFeed />
       <AboutPreview />
       <BooksEducation />
-      <LegalAuditModule />
       <ContactSection />
       <SiteFooter />
     </main>
