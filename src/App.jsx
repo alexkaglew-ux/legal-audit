85be48849552d26512358d8fb347152de75b6110
alexkaglew-ux <alex.kaglew@gmail.com>
2026-09-16 16:27:02 -0700

    Remove redundant What Alex Is Watching section

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import SiteHeader from "./components/SiteHeader.jsx";
 import Hero from "./components/Hero.jsx";
 import PracticeWorlds from "./components/PracticeWorlds.jsx";
-import WatchingRail from "./components/WatchingRail.jsx";
 import IssueExplorer from "./components/IssueExplorer.jsx";
 import InsightsFeed from "./components/InsightsFeed.jsx";
 import AboutPreview from "./components/AboutPreview.jsx";
@@ -15,7 +14,6 @@ export default function App() {
       <SiteHeader />
       <Hero />
       <PracticeWorlds />
-      <WatchingRail />
       <IssueExplorer />
       <InsightsFeed />
       <AboutPreview />
